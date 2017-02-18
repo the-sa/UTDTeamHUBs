53252e45c7d7ed0dda2081467be7530055b4dc2a
nxc122430 <nxc122430@utdallas.edu>
2017-02-17 19:14:43 -0600

    I was just trying to see if this commit works and how GitHub looks. I just added a button to change the textview when it is clicked. You can delete this when you work on it.

--- app/src/main/java/com/teamhub/utd/hub/MainActivity.java
+++ app/src/main/java/com/teamhub/utd/hub/MainActivity.java
@@ -2,12 +2,30 @@ package com.teamhub.utd.hub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // create variables
+    TextView text;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.textView);
+
+        // button to do something
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("Something was done...");
+            }
+        });
     }
 }
