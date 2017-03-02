293324945e9f4ab7c3ebdf6043f84765b9955d87
nxc122430 <nxc122430@utdallas.edu>
2017-03-01 20:41:34 -0600

    Added a different activity for scanning bluetooth (Still need to add a list of scanned devices) Added a floating button that starts activity and bluetooth scanning

--- app/src/main/java/com/teamhub/utd/hub/MainActivity.java
+++ app/src/main/java/com/teamhub/utd/hub/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.Tag;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,69 +18,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // create variables
-    BluetoothAdapter bluetoothAdapter;
-    Button button;
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                Log.i("Mac address:", deviceHardwareAddress + " " + deviceName);
-            }
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(mReceiver);
-    }
+    FloatingActionButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // set up bluetooth adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
         // button to do something
-        button = (Button)findViewById(R.id.button);
+        button = (FloatingActionButton) findViewById(R.id.floatingActionButton2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bluetoothOnOff();
+                Intent i = new Intent(MainActivity.this, UnPairedActivity.class);
+                startActivity(i);
             }
         });
-    }
-
-    private void bluetoothOnOff() {
-        if (bluetoothAdapter == null) {
-            Log.i("Error:", "Not working");
-        }
-        // if it is not enable then enable
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-
-            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mReceiver, BTIntent);
-        }
-        // if it is already enable then disable it
-        if(bluetoothAdapter.isEnabled()) {
-            bluetoothAdapter.disable();
-
-            IntentFilter BTIntent2 = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mReceiver, BTIntent2);
-        }
     }
 }
