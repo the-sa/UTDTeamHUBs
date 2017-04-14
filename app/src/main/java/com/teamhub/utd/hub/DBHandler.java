ccf4b4a450c9e7bb4908b8f620f5ce2f250797f4
gmg130030 <gmg130030@utdallas.edu>
2017-04-14 17:01:15 -0500

    Integrated firebase google services into the application

--- app/src/main/java/com/teamhub/utd/hub/DBHandler.java
+++ app/src/main/java/com/teamhub/utd/hub/DBHandler.java
@@ -12,7 +12,7 @@ public class DBHandler extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
     // Database Name
-    private static final String DATABASE_NAME = "RMDB.db";
+    private static final String DATABASE_NAME = "RMDB2.db";
 
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
