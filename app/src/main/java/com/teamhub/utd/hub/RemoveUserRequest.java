7353a0ee6e5d6a502a13c01b42248c11fbb94725
gmg130030 <gmg130030@utdallas.edu>
2017-04-21 13:58:10 -0500

    Added RemoveUserRequest code

--- app/src/main/java/com/teamhub/utd/hub/RemoveUserRequest.java
+++ app/src/main/java/com/teamhub/utd/hub/RemoveUserRequest.java
@@ -1,8 +1,18 @@
 package com.teamhub.utd.hub;
 
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
 /**
- * Created by Tiffany on 4/20/17.
+ * Created by Garrett on 4/20/17.
  */
 
-public class RemoveUserRequest {
+public class RemoveUserRequest extends StringRequest {
+    private static final String POPULATE_USERS_REQUEST_URL = "http://hueless-discharge.000webhostapp.com/RemoveUser.php";
+    //private Map< String, String> params;
+    public RemoveUserRequest(Response.Listener<String> listener) {
+        super(Method.POST, POPULATE_USERS_REQUEST_URL, listener, null);
+
+
+    }
 }
